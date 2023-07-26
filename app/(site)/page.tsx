8997458e3b7399206a054ff027e3f10de6fb6de3
midCenturyDude7 <mgriffes@gmail.com>
2023-07-25 19:52:00 -0700

    Working on Header and Button components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-      <Header>Header Inside</Header>
+      <Header>Header</Header>
     </div>
   );
 }
