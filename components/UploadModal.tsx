15479778ee728a6df36f8533bb2e69f4137082e1
midCenturyDude7 <mgriffes@gmail.com>
2023-08-05 09:27:37 -0700

    Working on Library component and associated functionality

---
+++ components/UploadModal.tsx
@@ -0,0 +1,15 @@
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  return (
+    <Modal
+      title="Upload Modal Title"
+      description="Upload Modal Description"
+      isOpen
+      onChange={() => {}}>
+      Upload Content
+    </Modal>
+  );
+};
+
+export default UploadModal;
