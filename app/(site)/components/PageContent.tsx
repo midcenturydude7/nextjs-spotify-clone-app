a22178aee17b0027dd64b62542db2b5d4d98ef2a
midCenturyDude7 <mgriffes@gmail.com>
2023-08-07 20:04:44 -0700

    Working on rendering a list of songs

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,14 +1,20 @@
-// "use client";
+"use client";
 
-import getSongs from "@/actions/getSongs";
+import SongItem from "@/components/SongItem";
+import { Song } from "@/types";
 
-const PageContent = async () => {
-  const songs = await getSongs();
+interface PageContentProps {
+  songs: Song[];
+}
+
+const PageContent: React.FC<PageContentProps> = ({ songs }) => {
+  if (songs.length === 0)
+    return <div className="mt-4 text-neutral-400">No songs yet</div>;
 
   return (
-    <div>
-      {songs.map((song, i) => (
-        <div key={i}>{song.title}</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
+      {songs.map((item) => (
+        <SongItem key={item.id} onClick={() => {}} data={item} />
       ))}
     </div>
   );
