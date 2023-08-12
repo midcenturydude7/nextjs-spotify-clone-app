45f70b4b0df8ede587ff1513138cb259479ef724
midCenturyDude7 <mgriffes@gmail.com>
2023-08-12 08:55:05 -0700

    Working on rendering list of songs to Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Figtree } from "next/font/google";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToastProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -16,11 +17,14 @@ export const metadata: Metadata = {
     "Built with React, Stripe, Supabase, PostgreSQL, Tailwing, and Vercel",
 };
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -28,7 +32,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>{children}</Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
