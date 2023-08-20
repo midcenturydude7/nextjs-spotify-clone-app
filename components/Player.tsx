bf06f1c1748c187eb735d9ae08d3ed0e12c515df
midCenturyDude7 <mgriffes@gmail.com>
2023-08-19 17:26:58 -0700

    Working on Player UI

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import PlayerContent from "./PlayerContent";
 
 import usePlayer from "@/hooks/usePlayer";
 import useGetSongById from "@/hooks/useGetSongById";
@@ -15,7 +16,7 @@ export const Player = () => {
 
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 };
