af24fb8481808a501207510fb42d9e06192da169
midCenturyDude7 <mgriffes@gmail.com>
2023-08-19 17:33:46 -0700

    Working on Player UI

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -3,9 +3,11 @@
 import React from "react";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
+import Slider from "./Slider";
 
 import { BsPlayFill, BsPauseFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2";
 
 import { Song } from "@/types";
 
@@ -16,6 +18,7 @@ interface PlayerContentProps {
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const Icon = true ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -38,7 +41,9 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
-        <div onClick={() => {}} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+        <div
+          onClick={() => {}}
+          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
@@ -46,6 +51,12 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
+      </div>
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon onClick={() => {}} size={34} className="cursor-pointer" />
+          <Slider />
+        </div>
       </div>
     </div>
   );
