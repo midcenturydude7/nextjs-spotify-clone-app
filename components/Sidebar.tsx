8358e110f3abf64f62793f4e7adc395fa611d8d9
midCenturyDude7 <mgriffes@gmail.com>
2023-08-22 20:26:46 -0700

    Updated Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,8 @@ import { usePathname } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { Song } from "../types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -15,6 +17,7 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
+  const player = usePlayer();
 
   const routes = React.useMemo(
     () => [
@@ -35,7 +38,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   );
 
   return (
-    <div className="flex h-full">
+    <div
+      className={twMerge(
+        `
+      flex
+      h-full
+    `,
+        player.activeId && "h-[calc(100%-80px)]"
+      )}>
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
