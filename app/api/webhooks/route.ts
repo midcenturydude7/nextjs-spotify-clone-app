c468fddc421df52faa178398134a1a8991c2adcf
midCenturyDude7 <mgriffes@gmail.com>
2023-09-07 21:07:50 -0700

    Minor edit

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -60,7 +60,7 @@ export async function POST(request: Request) {
         case "checkout.session.completed":
           const checkoutSession = event.data.object as Stripe.Checkout.Session;
           if (checkoutSession.mode === "subscription") {
-            const subscriptionId = checkoutSession.subscription;
+            const subscriptionId = checkutSession.subscription;
             await manageSubscriptionStatusChange(
               subscriptionId as string,
               checkoutSession.customer as string,
