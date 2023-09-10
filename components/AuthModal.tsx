4a663942ac760f6c70c2f26b7a36722d4e026225
midCenturyDude7 <mgriffes@gmail.com>
2023-09-10 10:06:07 -0700

    Fixed bug

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,26 +1,26 @@
 "use client";
-import React from "react";
 
+import React, { useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import {
   useSessionContext,
   useSupabaseClient,
 } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 import useAuthModal from "@/hooks/useAuthModal";
 
 import Modal from "./Modal";
-import { on } from "events";
 
 const AuthModal = () => {
-  const supabaseClient = useSupabaseClient();
-  const router = useRouter();
   const { session } = useSessionContext();
+  const router = useRouter();
   const { onClose, isOpen } = useAuthModal();
 
-  React.useEffect(() => {
+  const supabaseClient = useSupabaseClient();
+
+  useEffect(() => {
     if (session) {
       router.refresh();
       onClose();
@@ -35,21 +35,26 @@ const AuthModal = () => {
 
   return (
     <Modal
-      title="Welcome back!"
-      description="Login to your account"
+      title="Welcome back"
+      description="Login to your account."
       isOpen={isOpen}
       onChange={onChange}>
       <Auth
-        theme="dark"
-        providers={["github"]}
-        magicLink
         supabaseClient={supabaseClient}
+        providers={["github"]}
+        magicLink={true}
         appearance={{
           theme: ThemeSupa,
           variables: {
-            default: { colors: { brand: "#404040", brandAccent: "#22C55E" } },
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
           },
         }}
+        theme="dark"
       />
     </Modal>
   );
